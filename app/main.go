35ac39e80f91e77bf171929d570bdbc904acee86
Eric Li <eric.pireirik@gmail.com>
2021-11-14 20:36:29 +0100

    Fix cors for backend

--- app/main.go
+++ app/main.go
@@ -9,8 +9,20 @@ import (
 	"strconv"
 )
 
+func Cors(ctx iris.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	if ctx.Request().Method == "OPTIONS" {
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization")
+		ctx.StatusCode(204)
+		return
+	}
+	ctx.Next()
+}
+
 func main() {
 	app := iris.New()
+	app.Use(Cors)
 	app.Use(logger.New())
 	app.Use(recover.New())
 	app.Logger().SetLevel("debug")
