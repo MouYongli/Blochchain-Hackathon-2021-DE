5fdfc4de64dfee392df7c83bc1bbdc359f8801c1
Flexusma <info@flexusma.de>
2021-11-14 20:58:06 +0100

    Start resources page

--- frontend-vue/src/router/index.js
+++ frontend-vue/src/router/index.js
@@ -25,7 +25,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Modelview.vue')
-  }
+  },
+  {
+    path: '/resources',
+    name: 'Resources',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Resources.vue')
+  },
 ]
 
 const router = new VueRouter({
